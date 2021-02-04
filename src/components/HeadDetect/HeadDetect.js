0167fc91dd4da75b35264ff8824019050db0c770
Powered Automation <69073137+PoweredAutomation@users.noreply.github.com>
2021-02-04 12:00:42 +0530

    Update HeadDetect.js

--- src/components/HeadDetect/HeadDetect.js
+++ src/components/HeadDetect/HeadDetect.js
@@ -1,28 +1,76 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Component } from 'react'
 import MaterialTable from 'material-table'
 
 
 
-function HeadDetect(props) {
+class HeadDetect extends Component {
 
-  let FaceDataList = []
+  constructor(props) {
+    super(props);
+    this.state = {
+      FaceDataList: []
+    }
+  }
 
-  for(let i = 0; i < props.faceData.faces.length; i++){
+  FaceDataListNew = []
 
-    const item = props.faceData.faces[i];
+  componentDidMount(){
+    for(let i = 0; i < this.props.faceData.faces.length; i++){
 
-    let FaceData = {}
+      const item = this.props.faceData.faces[i];
+  
+      let FaceData = {}
+  
+      FaceData['object'] = <img src={item.object} alt='img' />
+      FaceData['time'] = item.starttime
+      FaceData['auto'] = item.auto ? 'Yes' : 'No'
+      FaceData['Manual'] = item.Manual ? 'Yes' : 'No'
+  
+      this.FaceDataListNew.push(FaceData)
+    }
+    // debugger;
+    console.log(this.FaceDataListNew)
 
-    FaceData['object'] = <img src={item.object} alt='img' />
-    FaceData['time'] = item.starttime
-    FaceData['auto'] = item.auto ? 'Yes' : 'No'
-    FaceData['Manual'] = item.Manual ? 'Yes' : 'No'
+    this.setState(
+      {
+        FaceDataList: this.FaceDataListNew
+      }
+    )
+  }
 
-    FaceDataList.push(FaceData)
+  BlurData = []
+
+  getRowData = (data) =>{
+    // debugger;
+    // let data1 = {
+    //   abc: "abc"
+    // }
+
+    for(let i=0; i < data.length; i++ ){
+      // debugger;
+      // console.log(data[0].object)
+      const item = data[i];
+      this.BlurData.push(
+        {
+          "url": item.object.props.src
+        }
+      )
+
+    }
+
+    let BlurAllData = {
+      "user_id": this.props.UserID,
+      "video_id": this.props.faceData.video_id,
+      "video_name": this.props.faceData.video_name,
+      "video_url": this.props.faceData.video_url,
+      "image_url": this.BlurData
+    }
+    
+    this.props.getSelectedRows(BlurAllData)
   }
 
   
-  const columns = [
+   columns = [
     {
       title: 'Object', field: 'object'
     },
@@ -37,22 +85,36 @@ function HeadDetect(props) {
     }
   ]
 
-  return (
-    <div>
-      <MaterialTable 
-      // title='Detected Faces'
-      data={FaceDataList}
-      columns={columns}
-      options={{
-        selection: true,
-        search: false,
-        paging: false,
-        showTextRowsSelected: false,
-        toolbar: false
-      }}
-      />
-    </div>
-  )
+  // handleSubmit = (data) => {
+  //   // const data = current.handleSubmit();
+  //   return data;
+  // }
+  
+  render(){
+    const {FaceDataList} = this.state
+    return (
+      <div>
+        <MaterialTable 
+        // title='Detected Faces'
+        data={FaceDataList}
+        columns={this.columns}
+        options={{
+          selection: true,
+          search: false,
+          paging: false,
+          showTextRowsSelected: true,
+          toolbar: false
+        }}
+        
+        onSelectionChange={(rowData) => this.getRowData(rowData)}
+        />
+      </div>
+    )
+  }
+
+  
 }
+
+
 
 export default HeadDetect
